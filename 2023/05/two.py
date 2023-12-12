6af90460b8ab464164912167f9dc978f21b8c54d
Joshua154 <joshineunert@gmail.com>
2023-12-12 05:18:08 +0100

    day 5

--- 2023/05/two.py
+++ 2023/05/two.py
@@ -1,25 +1,37 @@
+import threading
 from datetime import datetime
 
 from one import parseData, parseSeeds
 
+def threadFunction(data, seed, best):
+    seedMap = parseSeeds([seed], data)
+
+    if best == []:
+        best.append(seedMap[seed])
+    elif best[0]["humidity-to-location"] > seedMap[seed]["humidity-to-location"]:
+        best[0] = seedMap[seed]
 
 def main():
     start = datetime.now()
+    threads = []
     print("start at: " + str(datetime.now()))
     with open("./input.txt", "r", encoding="utf-8") as f:
         raw = f.read()
 
     tempSeeds, data = parseData(raw)
 
-    best = {}
+    best = []
     for i in range(0, len(tempSeeds), 2):
         for j in range(tempSeeds[i], tempSeeds[i] + tempSeeds[i + 1]):
-            seedMap = parseSeeds([j], data)
+            while len(threads) >= 50000:
+                for thread in threads:
+                    if not thread.is_alive():
+                        threads.remove(thread)
 
-            if best == {}:
-                best = seedMap[j]
-            elif best["humidity-to-location"] > seedMap[j]["humidity-to-location"]:
-                best = seedMap[j]
+            iThread = threading.Thread(target=threadFunction, args=(data, j, best))
+            iThread.start()
+            threads.append(iThread)
+
 
     print(best)
     print("end at: " + str(datetime.now()))
