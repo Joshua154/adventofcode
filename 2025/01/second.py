0a6c29ba4b98f99cc1276fe337461485abd13de0
Joshua Neunert <joshua.neunert@gmail.com>
2025-12-02 15:46:53 +0100

    fix: changes to function correctly

--- 2025/01/second.py
+++ 2025/01/second.py
@@ -1,36 +1,30 @@
-def sign(num):
-    if num > 0:
-        return 1
-    elif num < 0:
-        return -1
-    else:
-        return 0
 
 def main():
     with open("./input.txt", "r", encoding="utf-8") as f:
         raw = f.read()
 
     raw = raw.split("\n")
+    
     start = 50
-    passes_zero = 0
+    at_zero = 0
+    crosses = 0
 
-    for line in raw:
-        pre = start
-        dir, val = line[0], int(line[1:])
-        if dir == "R":
-            start += val
-        elif dir == "L":
-            start -= val
+    directions = [(-1 if line[0] == 'L' else 1) * int(line[1:]) for line in raw]
 
-        start %= 100
+    for steps in directions:
+        (multiple, remainder) = divmod(start + steps, 100)
 
-        print("move from", pre, "to", start)
+        if multiple < 0 and start == 0:
+            multiple += 1
 
-        if sign(pre) != sign(start):
-            print("passed zero")
-            passes_zero += 1
+        if multiple > 0 and remainder == 0:
+            multiple -= 1
+            
+        at_zero += 1 if remainder == 0 else 0
+        crosses += abs(multiple)
+        start = remainder
 
-    print(passes_zero)
+    print(at_zero + crosses)
 
 
 if __name__ == '__main__':
